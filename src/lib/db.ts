8b64ebc7a0edabb860931f80c78e463b1c70ffa2
okissibiri technology <michelleamfo2007@gmail.com>
2026-08-18 11:22:54 +0000

    Revert db.ts hacks

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client"
+import { Pool } from "pg"
+import { PrismaPg } from "@prisma/adapter-pg"
+
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+  return new PrismaClient({ adapter })
 }
 
 declare global {
