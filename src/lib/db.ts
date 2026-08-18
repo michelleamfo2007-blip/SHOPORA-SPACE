924d93d67bad58d397133820c749c1dbaf53b3ac
okissibiri technology <michelleamfo2007@gmail.com>
2026-08-18 11:11:29 +0000

    Fix database connection pooling on Vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,12 +2,10 @@ import { PrismaClient } from "@prisma/client"
 import { Pool } from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
 
-const connectionString = process.env.DATABASE_URL
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-
 const prismaClientSingleton = () => {
+  const connectionString = process.env.DATABASE_URL
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaPg(pool)
   return new PrismaClient({ adapter })
 }
 
